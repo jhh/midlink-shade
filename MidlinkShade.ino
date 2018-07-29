38083318d530d54fab32b478c9fa134a6bf6e3a7
Jeff Hutchison <jeff@jeffhutchison.com>
2018-07-29 11:17:35 -0400

    Add push button

--- MidlinkShade.ino
+++ MidlinkShade.ino
@@ -1,5 +1,4 @@
-#include <ESP8266WebServer.h>
-
+#import <Arduino.h>
 #include "Shade.hpp"
 #include "WiFi.hpp"
 #include "config.h"
@@ -8,12 +7,31 @@ using namespace midlink;
 
 Shade shade(15);
 
+int counter = 0;
+int state = LOW;
+int last_state = LOW;
+
 void setup() {
   Serial.begin(115200);
-  WiFi::Connect(SSID, WIFI_PASSWORD, "shades");
+  // WiFi::Connect(SSID, WIFI_PASSWORD, "shades");
+  pinMode(4, INPUT_PULLUP);
 }
 
 void loop() {
-  shade.Toggle();
-  delay(2000);
+  state = digitalRead(4);
+  
+  if (state != last_state) {
+    if (state == LOW) {
+      shade.Toggle();
+      Serial.println("button DOWN");
+      Serial.printf("button push counter = %d\n", ++counter);
+    } else {
+      Serial.println("button UP");
+    }
+    delay(50);
+    last_state = state;
+  }
+  
+
+  // delay(2000);
 }
